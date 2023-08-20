846da7ecd85aa30ab772ea06e61fcdc4b64285b5
Ashish Sharma <sharmaashish049@gmail.com>
2023-08-20 21:57:10 +0530

    Completed Now

---
+++ main.js
@@ -0,0 +1,4 @@
+let marqueeItems = document.querySelectorAll(".marquee-item");
+let fullMarqueeImg = document.querySelector(".full-marquee-img img");
+let closeBtn = document.querySelector(".closeBtn");
+let customModal = document.querySelector(".custom-modal");
